327424c91ef837928232e228fb7047bf898dda0f
Grigroriy Lozhkin <lozhkin.grig@gmail.com>
2023-10-22 18:52:26 +0300

    Update utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy as sp
 
 def find_nearest(list_l, value):
     """Finds an index of the nearest element of a list
@@ -55,5 +56,28 @@ def RE(targets,predictions,train_mean):
         reduction of error (RE)
   """
   return 1 - np.sum((targets-predictions)**2) / np.sum((targets-train_mean)**2)
+
+def direct_statistics(av_df, train_mean, clim_name='avg summer temperature'):
+    """Prints calibration and verification statistics
+
+      Parameters
+      ----------
+      av_df : pandas dataframe object
+          dataframe with predictions that averaged by the year
+      train_mean: float
+          mean of target values that was used in training
+      clim_name: str, optional:
+          name of column with observed values, default is
+          'avg summer temperature'
+    """
+    #Calibration and verification statistics
+    print('corr (Spearman): ', np.round(sp.stats.spearmanr(av_df[clim_name],
+                                                           av_df['preds'])[0],2))
+    print('CE: ', round(CE(av_df[clim_name],av_df['preds']),2))
+    print('RE: ', round(RE(av_df[clim_name],av_df['preds'],train_mean),2))
+    #Root Mean Square Error
+    RMSE = ((av_df[clim_name] - av_df['preds']) ** 2).mean() ** .5
+    print('RMSE: ', round(RMSE,2))
+
 
 
